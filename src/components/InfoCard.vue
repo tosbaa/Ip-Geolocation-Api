a8a4a2e43c8f7a8a4a83e10518507c51b58ea901
Yagiz Kocer <yagizkocer@gmail.com>
2021-03-08 20:17:49 +0300

    InfoCard component added

---
+++ src/components/InfoCard.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="info-card">
+    <div class="info-card__element">
+      <h6>IP ADDRESS</h6>
+      <p>192.212.174.101</p>
+    </div>
+    <div class="info-card__element">
+      <h6>LOCATION</h6>
+      <p>Brooklyn,NY 1001</p>
+    </div>
+    <div class="info-card__element">
+      <h6>TIMEZONE</h6>
+      <p>UTC-05:00</p>
+    </div>
+    <div class="info-card__element">
+      <h6>ISP</h6>
+      <p>SpaceX Starlink</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.info-card {
+  margin: auto;
+  width: 60%;
+  background-color: white;
+  border-radius: 3rem;
+  display: flex;
+  justify-content: center;
+}
+
+.info-card__element {
+  letter-spacing: 0.1ch;
+  color: black;
+  padding: 1rem 2rem;
+}
+
+.info-card__element:not(:first-child) {
+  border-left: 2px solid gray;
+}
+
+h6 {
+  color: gray;
+}
+
+p {
+  font-weight: 700;
+}
+</style>
