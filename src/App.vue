bc10d0f10b7dd5370d38ad400842a89740fb539b
Yagiz Kocer <yagizkocer@gmail.com>
2021-03-08 20:17:16 +0300

    Fonts added

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,16 @@ export default {
 <style>
 @import url("../public/styles/normalize.css");
 
+@font-face {
+  font-family: "Rubik";
+  src: url("../public/fonts/Rubik/Rubik-VariableFont_wght.ttf")
+      format("truetype"),
+    url("../public/fonts/Rubik/Rubik-Italic-VariableFont_wght.ttf")
+      format("truetype");
+  font-weight: 400 700;
+  unicode-range: U+0000- U+007F;
+}
+
 *,
 *::before,
 *::after {
@@ -19,9 +29,14 @@ export default {
 
 html {
   font-size: 10px;
+  font-family: "Rubik";
 }
 
 body {
+  font-size: 1.8rem;
+}
+
+#app {
   min-height: 100vh;
 }
 </style>
