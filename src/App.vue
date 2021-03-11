a8e60c3c3ae229e58dbee6fc036fc4b783cb100f
Yagiz Kocer <yagizkocer@gmail.com>
2021-03-11 16:14:19 +0300

    All logic removed from app and moved to vuex

--- src/App.vue
+++ src/App.vue
@@ -4,65 +4,14 @@
 </template>
 
 <script>
+import { mapActions } from "vuex";
 export default {
   name: "App",
-  data() {
-    return {
-      ipData: {
-        address: null,
-        region: null,
-        city: null,
-        coords: null,
-        postalCode: null,
-        timezone: null,
-        isp: null
-      }
-    };
-  },
-
   methods: {
-    async getIpOfClient() {
-      try {
-        const response = await fetch(
-          `https://cors-anywhere.herokuapp.com/${process.env.VUE_APP_IP_URL}`
-        );
-        if (!response.ok) {
-          throw new Error("Couldn't fetch the ip");
-        } else {
-          const data = await response.json();
-          return data.ip;
-        }
-      } catch (error) {
-        console.log(error.message);
-      }
-    },
-
-    async getIpDetails(ip) {
-      try {
-        const response = await fetch(
-          `https://cors-anywhere.herokuapp.com/${process.env.VUE_APP_GEO_URL}?apiKey=${process.env.VUE_APP_API_KEY}&ipAddress=${ip}`
-        );
-        if (!response.ok) {
-          throw new Error("Network error occured during fetch");
-        } else {
-          const data = await response.json();
-          return {
-            address: data.ip,
-            region: data.location.region,
-            city: data.location.city,
-            coords: [data.location.lat, data.location.lng],
-            postalCode: data.location.postalCode,
-            timezone: data.location.timezone,
-            isp: data.isp
-          };
-        }
-      } catch (error) {
-        console.log(error.message);
-      }
-    }
+    ...mapActions(["getIpOfClient"])
   },
-  async mounted() {
-    this.ipData = await this.getIpDetails(await this.getIpOfClient());
+  mounted() {
+    this.getIpOfClient();
   }
 };
 </script>
